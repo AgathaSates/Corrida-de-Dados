e581a62ad4609e3ad693eb187da658f70755391b
Agatha Sates <satesagatha@gmail.com>
2025-03-19 22:38:24 -0300

    Add project files.

---
+++ Corrida-de-Dados.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Corrida_de_Dados.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
